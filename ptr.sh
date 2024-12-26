b13340609b8895e123b95d5f7ad8f6ea1c61a82a
Artem <ahelp1@bk.ru>
2024-12-26 16:18:48 +0300

    Update ptr.sh

--- ptr.sh
+++ ptr.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-
-# Запрашиваем у пользователя имена файлов
-read -p "Введите имя файла для создания (file): " file
-read -p "Введите имя второго файла (file_2): " file_2
-
-# Проверяем, существует ли file_2
-if [[ ! -f "$file_2" ]]; then
-    echo "Файл $file_2 не существует."
-    exit 1
-fi
-
-# Создаем новый файл и копируем первые 8 строк из file_2
+read -p "PTR: " file
+read -p "A: "file_2
+read -p "domain name: " dn
+read -p "shablon seti (172.16): " subnet
 {
-    head -n 8 "$file_2"
-
-    # Обрабатываем строки после восьмой
-    tail -n +9 "$file_2" | while read -r line; do
-        if [[ "$line" == *" A "* ]]; then
-            # Извлекаем hostname и ip_address
-            hostname=$(echo "$line" | awk '{print $1}')
-            ip_address=$(echo "$line" | awk '{print $3}')
-
-            # Разворачиваем ip_address в нужный формат
-            IFS='.' read -r a b c d <<< "$ip_address"
-            echo "$d.$c.$b.$a PTR $hostname"
-        fi
-    done
+head -n 8 "$file_2"
+# Обрабатываем строки после восьмой
+tail -
+done
++10 "$file_2" I while read -r line; do if [[ $line == *"A"* ]]; then
+fi
 } > "$file"
-
+elements=($line)
+if [[ ${#elements[@]} -eq 3 ]]; then
+else
+fi
+hostname=$(echo $line | awk '{print $1}'); ip_address=$(echo $line | awk '{print $3}');
+hostname=$previous_hostname
+ip_address=$(echo $line | awk '{print $2}');
+if [[ "$ip_address" == $subnet* ]]; then previous_hostname=$hostname:
+IFS='.' read -r a b c d <<< "$ip_address"; echo "$d.$c.$b.$a PTR $hostname"."$dn"."";
+fi
+awk -v subnet="$subnet"
+{gsub(subnet".[0-9]{1,3}", ""); print}' file > file
 echo "Файл $file успешно создан."
