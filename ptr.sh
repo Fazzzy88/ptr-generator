79b0f9af70a642c2e2086ce0c6f3a1ecd6c9264f
Artem <ahelp1@bk.ru>
2024-12-26 16:25:48 +0300

    Update ptr.sh

--- ptr.sh
+++ ptr.sh
@@ -5,22 +5,21 @@ read -p "domain name: " dn
 read -p "shablon seti (172.16): " subnet
 {
 head -n 8 "$file_2"
-# Обрабатываем строки после восьмой
-tail -
-done
-+10 "$file_2" I while read -r line; do if [[ $line == *"A"* ]]; then
+
+tail -n  +10 "$file_2" | while read -r line; do 
+if [[ $line == *"A"* ]]; then
+  elements=($line)
+  if [[ ${#elements[@]} -eq 3 ]]; then
+    hostname=$(echo $line | awk '{print $1}'); 
+    ip_address=$(echo $line | awk '{print $3}');    
+  else
+    hostname=$previous_hostname
+    ip_address=$(echo $line | awk '{print $2}');
+  fi  
+  if [[ "$ip_address" == $subnet* ]]; then 
+  previous_hostname=$hostname;
+  IFS='.' read -r a b c d <<< "$ip_address"; 
+  echo "$d.$c.$b.$a PTR $hostname"."$dn"."";
 fi
 } > "$file"
-elements=($line)
-if [[ ${#elements[@]} -eq 3 ]]; then
-else
-fi
-hostname=$(echo $line | awk '{print $1}'); ip_address=$(echo $line | awk '{print $3}');
-hostname=$previous_hostname
-ip_address=$(echo $line | awk '{print $2}');
-if [[ "$ip_address" == $subnet* ]]; then previous_hostname=$hostname:
-IFS='.' read -r a b c d <<< "$ip_address"; echo "$d.$c.$b.$a PTR $hostname"."$dn"."";
-fi
-awk -v subnet="$subnet"
-{gsub(subnet".[0-9]{1,3}", ""); print}' file > file
 echo "Файл $file успешно создан."
