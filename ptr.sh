13395c01af0d68f8d0b866c88d752dddf5d8f20d
Artem <ahelp1@bk.ru>
2024-12-26 16:28:11 +0300

    Update ptr.sh

--- ptr.sh
+++ ptr.sh
@@ -7,19 +7,21 @@ read -p "shablon seti (172.16): " subnet
 head -n 8 "$file_2"
 
 tail -n  +10 "$file_2" | while read -r line; do 
-if [[ $line == *"A"* ]]; then
-  elements=($line)
-  if [[ ${#elements[@]} -eq 3 ]]; then
-    hostname=$(echo $line | awk '{print $1}'); 
-    ip_address=$(echo $line | awk '{print $3}');    
-  else
-    hostname=$previous_hostname
-    ip_address=$(echo $line | awk '{print $2}');
-  fi  
-  if [[ "$ip_address" == $subnet* ]]; then 
-  previous_hostname=$hostname;
-  IFS='.' read -r a b c d <<< "$ip_address"; 
-  echo "$d.$c.$b.$a PTR $hostname"."$dn"."";
-fi
+  if [[ $line == *"A"* ]]; then
+    elements=($line)
+    if [[ ${#elements[@]} -eq 3 ]]; then
+      hostname=$(echo $line | awk '{print $1}'); 
+      ip_address=$(echo $line | awk '{print $3}');    
+    else
+      hostname=$previous_hostname
+      ip_address=$(echo $line | awk '{print $2}');
+    fi  
+    if [[ "$ip_address" == $subnet* ]]; then 
+      previous_hostname=$hostname;
+      IFS='.' read -r a b c d <<< "$ip_address"; 
+      echo "$d.$c.$b.$a PTR $hostname"."$dn"."";
+    if
+  fi
+done
 } > "$file"
 echo "Файл $file успешно создан."
